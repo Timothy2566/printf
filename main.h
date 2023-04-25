8103ad932a025ce49de9c096aabb18526941388c
Timothy2566 <togunniyi5@gmail.com>
2023-04-25 07:31:24 -0700

    second update

--- main.h
+++ main.h
@@ -50,6 +50,11 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
+/****************** _printf ******************/
+
+void print_buffer(char buffer[], int *buff_ind);
+int _printf(const char *format, ...);
+
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
@@ -76,7 +81,7 @@ int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
+	char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
